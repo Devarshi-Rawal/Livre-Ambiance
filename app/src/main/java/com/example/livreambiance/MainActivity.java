e247a5bde70e8fab5390b77961ab27ee2c4651cc
Devarshi <jdsrawal558@gmail.com>
2022-01-15 00:55:04 +0530

    15/01/2021

--- app/src/main/java/com/example/livreambiance/MainActivity.java
+++ app/src/main/java/com/example/livreambiance/MainActivity.java
@@ -1,16 +1,33 @@
 package com.example.livreambiance;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    private int currentApiVersion;
+    private static final String TAG = "MainActivity";
+    ArrayList<String> listOfGenreNames = new ArrayList<>();
+    ArrayList<String> listOfGenreIcons = new ArrayList<>();
+
+    RecyclerView genreNamesRv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +41,56 @@ public class MainActivity extends AppCompatActivity {
         // TODO: 12-01-2022 Fullscreen working code ends
 
         setContentView(R.layout.activity_main);
+
+        genreNamesRv = findViewById(R.id.recyclerViewGn);
+
+        // TODO: 14-01-2022 Firebase config code starts
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference dbRefGenreNames = database.getReference("BookGenres").child("GenreNames");
+
+        DatabaseReference dbRefGenreIcons = database.getReference("BookGenres").child("GenreIcons");
+
+        Log.d(TAG, "onCreate: DB reference " + dbRefGenreNames);
+
+        dbRefGenreNames.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+//                String value = dataSnapshot.getValue(String.class);
+//                Log.d(TAG, "Value is: " + value);
+                for (DataSnapshot s: dataSnapshot.getChildren()) {
+                    listOfGenreNames.add(s.getValue().toString());
+                }
+                Log.d(TAG, "onDataChange: GenreNames "+ listOfGenreNames);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        dbRefGenreIcons.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot s : snapshot.getChildren()) {
+                    listOfGenreIcons.add(s.getValue().toString());
+                }
+                Log.d(TAG, "onDataChange: GenreIcons " + listOfGenreIcons);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        // TODO: 14-01-2022  Firebase config code ends
 
         /*// TODO: 12-01-2022 Snippet for fullscreen starts
 
